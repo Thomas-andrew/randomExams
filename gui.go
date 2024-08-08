43e3e9693a4c87ea4529c9be8110b6a2819e919e
thomas andrew <thomas.andrew.t6@gmail.com>
2024-08-07 23:54:53 -0300

    start day 080824

--- gui.go
+++ gui.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -49,4 +54,51 @@ func makeContent() fyne.CanvasObject {
 		content.Add(itemContainer)
 	}
 	return content
+}
+
+func makeExIngest(w fyne.Window) fyne.CanvasObject {
+	entry := widget.NewEntry()
+	entry.SetPlaceHolder("Enter number of screenshoots")
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "number", Widget: entry},
+		},
+		OnSubmit: func() {
+			num, err := strconv.Atoi(entry.Text)
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			var imgWidgets []fyne.CanvasObject
+			for i := 0; i < num; i++ {
+				path := fmt.Sprintf("./imgs/imageTest-%v.png", i)
+				err := screenshoot(path)
+				if err != nil {
+					dialog.ShowError(err, w)
+					return
+				}
+
+				image := canvas.NewImageFromFile(path)
+				image.SetMinSize(fyne.NewSize(200, 100))
+				imgWidgets = append(imgWidgets, image)
+			}
+
+			grid := container.New(layout.NewGridLayout(1), imgWidgets...)
+			w.SetContent(grid)
+		},
+	}
+	return form
+}
+
+func screenshoot(path string) error {
+	args := []string{"-s", "-m", "10", path}
+	cmd := exec.Command("maim", args...)
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("[screenshoot] maim finished with error: %w", err)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -8,18 +8,7 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Exercise Checklist")
 
-	w.SetContent(makeGUI())
+	form := makeExIngest(w)
+	w.SetContent(form)
 	w.ShowAndRun()
 }
-
-// func screenshoot(path string) error {
-// 	args := []string{"-s", "-m", "10", path}
-// 	cmd := exec.Command("maim", args...)
-//
-// 	err := cmd.Run()
-// 	if err != nil {
-// 		return fmt.Errorf("[screenshoot] maim finished with error: %w", err)
-// 	}
-//
-// 	return nil
-// }
